fcc586dd1859cb389c7197e272b88ebeea98302b
Liselle Logan <liselle.logan@gmail.com>
2020-09-25 01:56:41 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
-# code here!
+require 'pry'
+
+class School
+
+  attr_accessor :name
+  attr_reader :roster
+
+  def initialize (name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student_name, grade)
+    @roster[grade] = []
+    if @roster[grade]
+      @roster[grade] << student_name
+    else
+      @roster[grade] = [student_name]
+    end
+  end
+
+end
